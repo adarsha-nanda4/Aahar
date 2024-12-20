1a5da6f4c91faa2f906ae14304b224cf17ef1214
Adarsh Nanda <adarshananda51@gmail.com>
2024-12-21 03:43:03 +0530

    add btn

--- js/main.js
+++ js/main.js
@@ -11,3 +11,17 @@ BH.addEventListener("click",function(){
     localStorage.setItem("Hostel","BH")
     window.location.reload();
     })
+
+
+    // Select all items that have a submenu
+const menuItems = document.querySelectorAll('.menu .item');
+
+// Add click event listeners to toggle the submenu
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    const submenu = item.querySelector('.submenu');
+    if (submenu) {
+      submenu.classList.toggle('show'); // Toggle the "show" class
+    }
+  });
+});
